233755c2586e1a974249c3f9ae4821151bdaf671
Diogo Holanda <diogo_holanda2024@hotamail.com>
2024-11-12 14:31:40 -0300

    em andamento

--- cadastrar.php
+++ cadastrar.php
@@ -7,11 +7,11 @@ $nome = $_POST['nome_completo'];
 $cpf = $_POST['cpf'];
 $email = $_POST['email'];
 $telefone = $_POST['telefone'];
-$setor = $_POST['setor_resp'];
+$setor = $_POST['setor_responsavel'];
 $detalhes = $_POST['detalhes'];
 // 1º Passo - Comando SQL
-$sql = "INSERT INTO tb_protocolos
-        (nome_completo, cpf, email, telefone, setor_resp, detalhes) VALUES
+$sql = "INSERT INTO tb_protocolo
+        (nome_completo, cpf, email, telefone, setor_responsavel, detalhes) VALUES
         ('$nome', '$cpf','$email', '$telefone', '$setor', '$detalhes')";
 // 2º Passo- Preparar a conexão
 $inserir = $pdo->prepare($sql);
--- consultar.php
+++ consultar.php
@@ -1,8 +1,8 @@
 <?php
 include "conexao.php";
 $cod = $_POST['cod'];
-$sql = "SELECT * FROM tb_protocolos
-        WHERE nome_completo LIKE '%$cod%'";
+$sql = "SELECT * FROM tb_protocolo
+        WHERE numero_protocolo LIKE '%$cod%'";
 
 $consultar = $pdo->prepare($sql);
 try{
@@ -10,15 +10,17 @@ try{
     if($consultar->rowCount()>=1){
         $resultado = $consultar->fetchAll(PDO::FETCH_ASSOC);
         foreach ($resultado as $item){
+            $num = $item['numero_protocolo'];
             $nome = $item['nome_completo'];
             $cpf = $item['cpf'];
             $email = $item['email'];
             $telefone = $item['telefone'];
-            $setor = $item['setor_resp'];
+            $setor = $item['setor_responsavel'];
             $detalhes = $item['detalhes'];
     
             echo "
                     <div class='atualizacao'>           
+                        <span class='numero_protocolo'>Nº: $num</span> <br>           
                         <span class='nome_completo'>Nome: $nome</span> <br>           
                         <span class='cpf'>CPF: $cpf</span> <br>           
                         <span class='email'>E-mail: $email</span> <br>           
